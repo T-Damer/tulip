0ca935f31e42e52b6cde640298ae89543b6ac8d8
T_DaMER <dpancov64@gmail.com>
2025-06-01 12:07:16 +0300

    Less-round tags

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 export default function Tag({ children }: PropsWithChildren) {
   return (
-    <div className="rounded-2xl border px-1 text-xs whitespace-nowrap opacity-50">
+    <div className="rounded-xs border px-1 text-xs whitespace-nowrap opacity-50">
       {children}
     </div>
   )
